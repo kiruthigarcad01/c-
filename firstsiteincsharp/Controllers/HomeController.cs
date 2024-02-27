e88cd2f695ec9acc44a6847a0049fab02b92a5c9
kiruthigarcad01 <kiruthigarcad01@gmail.com>
2024-02-27 15:10:02 +0530

    i have updated mvc menus

---
+++ firstsiteincsharp/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using firstsiteincsharp.Models;
+
+namespace firstsiteincsharp.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+    public IActionResult Admin()
+    {
+        return View();
+    }
+    public IActionResult Course()
+    {
+        return View();
+    }
+    public IActionResult Librarian()
+    {
+        return View();
+    }
+    public IActionResult Student()
+    {
+        return View();
+    }
+    public IActionResult Contact()
+    {
+        return View();
+    }
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ firstsiteincsharp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace firstsiteincsharp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
