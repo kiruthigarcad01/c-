efdeb7cc539b040aa672108319d99c3d8c815db3
kiruthigarcad01 <kiruthigarcad01@gmail.com>
2024-02-27 17:44:47 +0530

    added interface

---
+++ interface1/Program.cs
@@ -0,0 +1,76 @@
+﻿
+    public interface Drawable{
+        void draw();
+    }
+
+
+public interface SetDrawable{
+    void SetColour();
+
+
+}
+
+    public class Circle : Drawable, SetDrawable{
+        public void draw(){
+            Console.WriteLine("i can draw circle");
+        }
+
+        public void SetColour(){
+            Console.WriteLine("i can do colour for circle");
+        }
+    }
+
+    
+
+    public class Square : Drawable{
+        public void draw(){
+            Console.WriteLine("I can draw Square");
+        }
+    }
+
+    public class Traingle : Drawable{
+        public void draw(){
+            Console.WriteLine("i can draw Traingle");
+        }
+    }
+
+    public class Rectangle : Drawable{
+        public void draw(){
+            Console.WriteLine("i can draw rectangle");
+        }
+    }
+
+    public class Line : Drawable{
+        public void draw(){
+            Console.WriteLine("i can draw line");
+            
+        }
+    }
+
+namespace interface1{
+
+        public class Program{
+
+
+    public static void Main(string[] args){
+        Drawable d;
+        SetDrawable sd;
+        
+        d=new Circle();
+        d.draw();
+        sd=new Circle();
+        sd.SetColour();
+
+        d=new Square();
+        d.draw();
+        d=new Traingle();
+        d.draw();
+        d=new Rectangle();
+        d.draw();
+        Console.ReadLine();
+        Line l=new Line();
+        l.draw();
+    }
+    }
+    }
+
