0201605f50336b5b22e990866a79a5a77d96fda1
kiruthigarcad01 <kiruthigarcad01@gmail.com>
2024-03-13 18:12:35 +0530

    mynewpro

---
+++ newmvcsharp/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using newmvcsharp.Models;
+using Microsoft.Data.SqlClient;
+
+namespace newmvcsharp.Controllers;
+
+public class HomeController : Controller
+{
+    SqlConnection con=new SqlConnection();
+    SqlCommand cmd=new SqlCommand();
+    SqlDataReader? dr;
+
+
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+    public IActionResult login()
+    {
+        return View();
+    }
+
+    void ConnectionString(){
+        con.ConnectionString="192.168.1.240\\SQLEXPRESS; database=cad_foodhub; integrated security = SSIP; TrustServerCertificate=True;";
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ newmvcsharp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace newmvcsharp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
